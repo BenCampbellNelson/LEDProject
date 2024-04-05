f762bd6296b0731aa150d91ef864eef3f009a611
BenCampbellNelson <campbellnelsonb@gmail.com>
2024-04-05 17:02:39 +1300

    Add variables

--- LED_Project/LED_Project.ino
+++ LED_Project/LED_Project.ino
@@ -1,3 +1,12 @@
+int red = 0;
+int green = 0;
+int blue = 0;
+int redIn = 5;
+int redOut = 11;
+int greenIn = 6;
+int greenOut = 12;
+int blueIn = 7;
+int blueOut = 13;
 void setup() {
   // put your setup code here, to run once:
 
